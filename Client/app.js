970d872ce3e86dbf2dc7bc232f174ae4d1895c20
NicholasthedevJacobs <jjacobs.nicholas@gmail.com>
2020-10-01 10:27:49 -0500

    Added new functions to house the ajax methods.  Also changed up html to correctly log events.

--- Client/app.js
+++ Client/app.js
@@ -1,18 +1,49 @@
-// $(function($){
+function searchBYMovieId(){
+    (function($){
+        function processForm( e ){
+            var dict = {
+            
+                MovieId: this["movieID"].value
+            };
+    
+            
+    
+            $.ajax({
+                url: 'https://localhost:44325/api/movie',
+                dataType: "json",           
+                type: 'get',
+                contentType: 'application/json',
+                data: JSON.stringify(dict),
+                success: function( data, textStatus, jQxhr ){
+                    $('#response pre').html( data );
+                },
+                error: function( jqXhr, textStatus, errorThrown ){
+                    console.log( errorThrown );
+                }
+            });
+    
+            e.preventDefault();
+        }
+    
+        
+    
+        $('#my-movie-search').submit( processForm );
+    })(jQuery);
+}
+
+// (function($){
 //     function processForm( e ){
 //         var dict = {
-//         	Title : this["title"].value,
-//             Director: this["director"].value,
-//             Genre: this["genre"].value,
-          
+        
+//             MovieId: this["movieID"].value
 //         };
 
-      
+        
 
 //         $.ajax({
 //             url: 'https://localhost:44325/api/movie',
-//             dataType: 'json',
-//             type: 'post',
+//             dataType: "json",           
+//             type: 'get',
 //             contentType: 'application/json',
 //             data: JSON.stringify(dict),
 //             success: function( data, textStatus, jQxhr ){
@@ -28,72 +59,104 @@
 
     
 
-//     $('#Add-Movie').submit( processForm );
+//     $('#my-movie-search').submit( processForm );
 // })(jQuery);
+//document.getElementById("AddMovie").addEventListener("click", addMovie);
 
-(function($){
-    function processForm( e ){
-        var dict = {
+function addMovie(e){
+    // document.getElementById("AddMovie").innerHTML = 
+            //console.log(e);
+    
+            var dict = {
+            
+                Title : document.getElementById("title").value,
+                 Director: document.getElementById("director").value,
+                 Genre: document.getElementById("genre").value,
+            };
+                //console.log("dict", dict);   
+            $.ajax({
+                url: 'https://localhost:44325/api/movie',
+                dataType: "json",           
+                type: 'post',
+                contentType: 'application/json',
+                data: JSON.stringify(dict),
+                success: function( data, textStatus, jQxhr ){
+                    // $('#response pre').html( data );
+                    console.log(data);
+                },
+                error: function( jqXhr, textStatus, errorThrown ){
+                    console.log( errorThrown );
+                }
+            });
+    
+            e.preventDefault();
+        }
+
+        function updateMovie(e){
+            // document.getElementById("AddMovie").innerHTML = 
+                    //console.log(e);
+            
+                    var dict = {
+                    
+                        Title : document.getElementById("title").value,
+                         Director: document.getElementById("director").value,
+                         Genre: document.getElementById("genre").value,
+                    };
+                        //console.log("dict", dict);   
+                    $.ajax({
+                        url: 'https://localhost:44325/api/movie',
+                        dataType: "json",           
+                        type: 'put',
+                        contentType: 'application/json',
+                        data: JSON.stringify(dict),
+                        success: function( data, textStatus, jQxhr ){
+                            // $('#response pre').html( data );
+                            console.log(data);
+                        },
+                        error: function( jqXhr, textStatus, errorThrown ){
+                            console.log( errorThrown );
+                        }
+                    });
+            
+                    e.preventDefault();
+                }
         
-            MovieId: this["movieID"].value
-        };
+    
+           
+
+
+// (function($){
+//     function processForm( e ){
+//         var dict = {
+        
+//             Title : this["title"].value,
+//              Director: this["director"].value,
+//              Genre: this["genre"].value,
+//         };
 
         
 
-        $.ajax({
-            url: 'https://localhost:44325/api/movie',
-            dataType: "json",           
-            type: 'get',
-            contentType: 'application/json',
-            data: JSON.stringify(dict),
-            success: function( data, textStatus, jQxhr ){
-                $('#response pre').html( data );
-            },
-            error: function( jqXhr, textStatus, errorThrown ){
-                console.log( errorThrown );
-            }
-        });
+//         $.ajax({
+//             url: 'https://localhost:44325/api/movie',
+//             dataType: "json",           
+//             type: 'put',
+//             contentType: 'application/json',
+//             data: JSON.stringify(dict),
+//             success: function( data, textStatus, jQxhr ){
+//                 $('#response pre').html( data );
+//             },
+//             error: function( jqXhr, textStatus, errorThrown ){
+//                 console.log( errorThrown );
+//             }
+//         });
 
-        e.preventDefault();
-    }
+//         e.preventDefault();
+//     }
 
     
 
-    $('#my-movie-search').submit( processForm );
-})(jQuery);
-
-(function($){
-    function processForm( e ){
-        var dict = {
-        
-            Title : this["title"].value,
-             Director: this["director"].value,
-             Genre: this["genre"].value,
-        };
-
-        
-
-        $.ajax({
-            url: 'https://localhost:44325/api/movie',
-            dataType: "json",           
-            type: 'put',
-            contentType: 'application/json',
-            data: JSON.stringify(dict),
-            success: function( data, textStatus, jQxhr ){
-                $('#response pre').html( data );
-            },
-            error: function( jqXhr, textStatus, errorThrown ){
-                console.log( errorThrown );
-            }
-        });
-
-        e.preventDefault();
-    }
-
-    
-
-    $('#Update-Movie').submit( processForm );
-})(jQuery);
+//     $('#Update-Movie').submit( processForm );
+// })(jQuery);
 
 (function($){
     function processForm( e ){
@@ -129,18 +192,19 @@
     $('#Delete-Movie').submit( processForm );
 })(jQuery);
 
-$(function($){
+// function updateMovie(){
+//     $(function($){
 
-    $.post(`https://localhost:44325/api/movie`, function(data){
-        console.log(data);
-    
-        data.map(function(el){
-            $("#Add-Movie").append(`<div> Movie Title: ${el.title} </div>
-            <div> Movie Director: ${el.director}</div>
-            <div> Genre: ${el.genre}</div>
-            <br>`);
-        })
-    
-    })
-});
-
+//         $.post(`https://localhost:44325/api/movie`, function(data){
+//             console.log(data);
+        
+//             data.map(function(el){
+//                 $("#Add-Movie").append(`<div> Movie Title: ${el.title} </div>
+//                 <div> Movie Director: ${el.director}</div>
+//                 <div> Genre: ${el.genre}</div>
+//                 <br>`);
+//             })
+        
+//         })
+//     });
+// }
